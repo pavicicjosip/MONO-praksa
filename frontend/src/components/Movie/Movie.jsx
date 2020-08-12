f5b381594ff9087be056f54efbe187f353c6b06c
rerceg <roko.erceg610@gmail.com>
2020-08-12 21:48:10 +0200

    Movie component begining

--- frontend/src/components/Movie/Movie.jsx
+++ frontend/src/components/Movie/Movie.jsx
@@ -1,13 +1,42 @@
 import React, { Component } from "react";
+import axios from "axios";
+import { decorate, observable } from "mobx";
+import { observer } from "mobx-react";
 
-class Movie extends Component {
-  render() {
-    return (
-      <div>
-        <p>{this.props.movie.Title}</p>
-      </div>
-    );
+const Movie = observer(
+  class Movie extends Component {
+    path = require("../../Assets/Images/Movies/DefaultMovieImage.png");
+
+    constructor(props) {
+      super(props);
+      this.getImage();
+    }
+
+    getImage = async () => {
+      await axios
+        .get(
+          "https://localhost:44336/api/FileStorage?fileID=" +
+            this.props.movie.FileID
+        )
+        .then((response) => {
+          console.log(response.data);
+          this.path = require("../../Assets/" + response.data);
+        });
+    };
+
+    render() {
+      return (
+        <div>
+          <p>{this.props.movie.Title}</p>
+          <img src={this.path} alt="movie" height="200px" width="300px" />;
+        </div>
+      );
+    }
   }
-}
+);
+
+decorate(Movie, {
+  path: observable,
+});
 
 export default Movie;
