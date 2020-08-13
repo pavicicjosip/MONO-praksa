52bbf7bc35e1a075cc6d8194c077994a56695a47
rerceg <roko.erceg610@gmail.com>
2020-08-13 21:56:03 +0200

    HomePage fix

--- frontend/src/components/HomePage/HomePage.jsx
+++ frontend/src/components/HomePage/HomePage.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import Movies from "../Movie/Movies";
 import { decorate, observable } from "mobx";
 import { observer } from "mobx-react";
-import './HomePage.scss';
+import "./HomePage.scss";
 import { Link } from "react-router-dom";
 
 const HomePage = observer(
@@ -11,9 +11,11 @@ const HomePage = observer(
     movies = [];
 
     componentDidMount() {
-      axios.get("https://localhost:44336/api/Movie").then((response) => {
-        this.movies = response.data;
-      });
+      axios
+        .get("https://localhost:44336/api/Movie?pageSize=5")
+        .then((response) => {
+          this.movies = response.data;
+        });
     }
 
     render() {
@@ -23,14 +25,22 @@ const HomePage = observer(
       }
       return (
         <div>
-          <p><img src={require("./b.png")} className="center" alt=""/></p>
-          <h1 className='centerd'>Welcome...</h1>
-          <h3 className='centerd1'>You've found the most accurate source for TV and film. Our information comes from fans like you, so create a free account and help your favorite shows and movies.</h3>
-          <h2 className='title'>Featured Movies</h2>
-          <div className='spacer'></div>
+          <p>
+            <img src={require("./b.png")} className="center" alt="" />
+          </p>
+          <h1 className="centerd">Welcome...</h1>
+          <h3 className="centerd1">
+            You've found the most accurate source for TV and film. Our
+            information comes from fans like you, so create a free account and
+            help your favorite shows and movies.
+          </h3>
+          <h2 className="title">Featured Movies</h2>
+          <div className="spacer"></div>
           <Movies movies={moviesProp} />
           <div>
-          <Link to="/movieInfoPage/99120BAA-5B4C-46CB-A1A2-0805930A0EE9">PROBA FILM</Link>
+            <Link to="/movieInfoPage/99120BAA-5B4C-46CB-A1A2-0805930A0EE9">
+              PROBA FILM
+            </Link>
           </div>
         </div>
       );
