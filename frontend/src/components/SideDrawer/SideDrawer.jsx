3fa5b8ab70b1b3ea9294235a32943a3dc451da49
rerceg <roko.erceg610@gmail.com>
2020-08-17 15:38:02 +0200

    Movies, HomePage adjustment, LogOut

--- frontend/src/components/SideDrawer/SideDrawer.jsx
+++ frontend/src/components/SideDrawer/SideDrawer.jsx
@@ -1,15 +1,21 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
-import './SideDrawer.scss';
+import "./SideDrawer.scss";
 
-const sideDrawer = props => (
-    <nav className="side-drawer">
-        <ul>
-            <li><a href="/">Movies</a></li>
-            <li><a href="/">Celebs</a></li>
-
-        </ul>
-    </nav>
+const sideDrawer = (props) => (
+  <nav className="side-drawer">
+    <ul>
+      <li>
+        <Link to="/movies" onClick={props.click}>
+          Movies
+        </Link>
+      </li>
+      <li>
+        <a href="/">Celebs</a>
+      </li>
+    </ul>
+  </nav>
 );
 
 export default sideDrawer;
